a83cb9f0aa357d55af4315fef7e045429708aaf1
Welisson Silveira <welissonmwse@gmail.com>
2022-12-20 10:43:34 -0300

    Estrutura da aplicação - Instalando o NativeBase

--- App.tsx
+++ App.tsx
@@ -1,18 +1,19 @@
 import { StatusBar, Text, View } from 'react-native';
+import { NativeBaseProvider } from 'native-base'
 import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
 
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
 
   return (
-    <View>
+    <NativeBaseProvider>
       <StatusBar 
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
       <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    </NativeBaseProvider>
   );
 }
 
