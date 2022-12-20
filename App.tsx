5449b2f90f8c5547f0da21ff1fb68804f48d911a
Welisson Silveira <welissonmwse@gmail.com>
2022-12-20 11:11:13 -0300

    Estrutura da aplicação - Configurando o EsLint

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
-import { StatusBar, Text } from 'react-native';
-import { NativeBaseProvider } from 'native-base'
-import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
-import {Loading} from '@components/Loading'
+import { StatusBar } from 'react-native';
+import { NativeBaseProvider } from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
+import {Loading} from '@components/Loading';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
 
   return (
     <NativeBaseProvider>
-      <StatusBar 
+      <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
