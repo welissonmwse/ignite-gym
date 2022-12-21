abb8f7b465975361e72fa35c2c6d73943289aec4
Welisson Silveira <welissonmwse@gmail.com>
2022-12-21 09:36:52 -0300

    Estrutura visual - Componente Input

---
+++ src/components/Input.tsx
@@ -0,0 +1,23 @@
+import { Input as NativeBaseInput, IInputProps} from 'native-base';
+
+
+export function Input({...props}: IInputProps){
+  return(
+    <NativeBaseInput
+      {...props}
+      bg="gray.700"
+      h={16}
+      px={4}
+      mb={4}
+      borderWidth={0}
+      color="white"
+      fontFamily="body"
+      placeholderTextColor="gray.300"
+      _focus={{
+        bg: 'gray.700',
+        borderWidth: 1,
+        borderColor: 'green.500'
+      }}
+    />
+  );
+}
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,11 +1,13 @@
 import { Center, Heading, Image, Text, VStack } from 'native-base';
 
+import { Input } from '@components/Input';
+
 import backgroundImg from '@assets/background.png';
 import LogoSvg from '@assets/logo.svg';
 
 export function SignIn(){
   return(
-    <VStack flex={1} bg="gray.700">
+    <VStack flex={1} bg="gray.700" px={10}>
       <Image source={backgroundImg} alt="Pessoas treinando" resizeMode="contain" position="absolute"/>
       <Center my={24}>
         <LogoSvg />
@@ -17,6 +19,15 @@ export function SignIn(){
         <Heading color="gray.100" fontSize="xl" fontFamily="heading" mb={6}>
         Acesse sua conta
         </Heading>
+        <Input
+          placeholder="E-mail"
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+        <Input
+          placeholder="Senha"
+          secureTextEntry
+        />
       </Center>
     </VStack>
   );
