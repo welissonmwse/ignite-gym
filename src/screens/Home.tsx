c7da2ca767ea6abb408996fa80e0fdcf754f6d74
Welisson Silveira <welissonmwse@gmail.com>
2022-12-23 09:40:12 -0300

    Estrutura visual - Criando componente Group

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,27 @@
-import { VStack } from 'native-base';
+import { useState } from 'react';
+import { HStack, VStack } from 'native-base';
 
 import { HomeHeader } from '@components/HomeHeader';
+import { Group } from '@components/Group';
 
 export function Home(){
+  const [groupSelected, setGroupSelected] = useState('Costa');
+
   return(
     <VStack flex={1}>
       <HomeHeader />
+      <HStack>
+        <Group
+          name="Costa"
+          isActive={groupSelected === 'Costa'}
+          onPress={() => setGroupSelected('Costa')}
+        />
+        <Group
+          name="Ombro"
+          isActive={groupSelected === 'Ombro'}
+          onPress={() => setGroupSelected('Ombro')}
+        />
+      </HStack>
     </VStack>
   );
 }
