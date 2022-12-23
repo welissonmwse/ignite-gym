c8b17f9741477bccd35590b4c27be374af75e98d
Welisson Silveira <welissonmwse@gmail.com>
2022-12-23 09:56:34 -0300

    Estrutura visual - Header da lista de exercícios

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { FlatList, VStack } from 'native-base';
+import { FlatList, Heading, HStack, Text, VStack } from 'native-base';
 
 import { HomeHeader } from '@components/HomeHeader';
 import { Group } from '@components/Group';
@@ -27,6 +27,16 @@ export function Home(){
         my={10}
         maxH={10}
       />
+      <VStack flex={1} px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            Exercícios
+          </Heading>
+          <Text color="gray.200" fontSize="sm">
+            4
+          </Text>
+        </HStack>
+      </VStack>
     </VStack>
   );
 }
