79078da8a0d0bd9be644933b7c92adad8e8dd7e0
Welisson Silveira <welissonmwse@gmail.com>
2022-12-23 08:50:46 -0300

    Navegação - Aplicando navegação de rotas privadas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { useTheme } from 'native-base';
-import { AuthRoutes } from './auth.routes';
+import { AppRoutes } from './app.routes';
+// import { AuthRoutes } from './auth.routes';
 
 export function Routes(){
   const {colors} = useTheme();
@@ -10,7 +11,7 @@ export function Routes(){
 
   return(
     <NavigationContainer theme={theme}>
-      <AuthRoutes />
+      <AppRoutes />
     </NavigationContainer>
   );
 }
