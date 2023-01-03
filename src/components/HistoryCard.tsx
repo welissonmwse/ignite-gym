762eed7f313342513e224152950b3b7707e8f23b
Welisson Silveira <welissonmwse@gmail.com>
2023-01-03 10:14:00 -0300

    Estrutura visual - Ajustes finais nas interfaces

--- src/components/HistoryCard.tsx
+++ src/components/HistoryCard.tsx
@@ -3,8 +3,8 @@ import { Heading, HStack, Text, VStack } from 'native-base';
 export function HistoryCard(){
   return(
     <HStack w="full" px={5} py={4} mb={3} bg="gray.600" rounded="md" alignItems="center" justifyContent="space-between">
-      <VStack mr={5}>
-        <Heading color="white" fontSize="md" textTransform="capitalize">
+      <VStack flex={1} mr={5}>
+        <Heading color="white" fontSize="md" textTransform="capitalize" numberOfLines={1}>
           Costas
         </Heading>
         <Text color="gray.100" fontSize="lg" numberOfLines={1}>Puxada frontal</Text>
--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,5 +1,5 @@
 import { TouchableOpacity } from 'react-native';
-import { Box, Heading, HStack, Icon, Image, Text, VStack } from 'native-base';
+import { Box, Heading, HStack, Icon, Image, ScrollView, Text, VStack } from 'native-base';
 
 import { useNavigation } from '@react-navigation/native';
 import { Feather } from '@expo/vector-icons';
@@ -37,34 +37,36 @@ export function Exercise(){
           </HStack>
         </HStack>
       </VStack>
-      <VStack p={8}>
-        <Image
-          source={{uri: 'http://conteudo.imguol.com.br/c/entretenimento/0c/2019/12/03/remada-unilateral-com-halteres-1575402100538_v2_600x600.jpg'}}
-          alt="Image do exercício"
-          w="full"
-          h={80}
-          mb={3}
-          resizeMode="cover"
-          rounded="lg"
-        />
-        <Box bg="gray.600" rounded="md" pb={4} px={4}>
-          <HStack alignItems="center" justifyContent="space-around" mb={6} mt={5}>
-            <HStack>
-              <SeriesSvg />
-              <Text color="gray.200" ml={2}>
-                3 séries
-              </Text>
+      <ScrollView>
+        <VStack p={8}>
+          <Image
+            source={{uri: 'http://conteudo.imguol.com.br/c/entretenimento/0c/2019/12/03/remada-unilateral-com-halteres-1575402100538_v2_600x600.jpg'}}
+            alt="Image do exercício"
+            w="full"
+            h={80}
+            mb={3}
+            resizeMode="cover"
+            rounded="lg"
+          />
+          <Box bg="gray.600" rounded="md" pb={4} px={4}>
+            <HStack alignItems="center" justifyContent="space-around" mb={6} mt={5}>
+              <HStack>
+                <SeriesSvg />
+                <Text color="gray.200" ml={2}>
+                  3 séries
+                </Text>
+              </HStack>
+              <HStack>
+                <RepetitionsSvg />
+                <Text color="gray.200" ml={2}>
+                  12 repetições
+                </Text>
+              </HStack>
             </HStack>
-            <HStack>
-              <RepetitionsSvg />
-              <Text color="gray.200" ml={2}>
-                12 repetições
-              </Text>
-            </HStack>
-          </HStack>
-          <Button title="Marcar como realizado" />
-        </Box>
-      </VStack>
+            <Button title="Marcar como realizado" />
+          </Box>
+        </VStack>
+      </ScrollView>
     </VStack>
   );
 }
