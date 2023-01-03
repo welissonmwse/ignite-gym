8f03fc90f508214fcc753c0808e34048a57ec9f0
Welisson Silveira <welissonmwse@gmail.com>
2023-01-03 08:42:00 -0300

    Estrutura visual - Mensagem de lista vazia

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,4 +1,4 @@
-import { Heading, SectionList, VStack } from 'native-base';
+import { Heading, SectionList, Text, VStack } from 'native-base';
 
 import { HistoryCard } from '@components/HistoryCard';
 import { ScreenHeader } from '@components/ScreenHeader';
@@ -30,6 +30,13 @@ export function History(){
           </Heading>
         )}
         px={8}
+        contentContainerStyle={exercises.length === 0 && { flex: 1, justifyContent: 'center'}}
+        ListEmptyComponent={() => (
+          <Text color="gray.100" textAlign="center">
+            Não há exercícios registrados ainda. {'\n'}
+            Vamos fazer exercícios hoje?
+          </Text>
+        )}
       />
     </VStack>
   );
