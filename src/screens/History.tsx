e893db5c248906faabc52564cce106b199cb02ea
Welisson Silveira <welissonmwse@gmail.com>
2023-01-06 11:27:24 -0300

    Conexão com a API - Carregando histórico

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,9 +1,15 @@
-import { Heading, SectionList, Text, VStack } from 'native-base';
+import { useEffect, useState } from 'react';
+import { Heading, SectionList, Text, useToast, VStack } from 'native-base';
+
+import { api } from '@services/api';
+import { AppError } from '@utils/AppError';
 
 import { HistoryCard } from '@components/HistoryCard';
 import { ScreenHeader } from '@components/ScreenHeader';
 
 export function History(){
+  const [isLoading, setIsLoading] = useState(true);
+  // const [exercises, setExercises] = useState([]);
   const exercises = [
     {
       title: '26.08.23',
@@ -14,6 +20,34 @@ export function History(){
       data: ['Puxada frontal']
     },
   ];
+
+  const toast = useToast();
+
+  async function fetchHistory(){
+    try {
+      setIsLoading(true);
+      const response = await api.get('/history');
+      // setExercises(response.data);
+      console.log(response.data[0]);
+      // const data = response.data.map()
+
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError ? error.message : 'Não foi possível carregar o histórico de exercícios';
+
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchHistory();
+  }, []);
 
   return(
     <VStack flex={1}>
