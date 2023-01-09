c339b85f57af7a6508ef58977c9e35ed8b48b2b6
Welisson Silveira <welissonmwse@gmail.com>
2023-01-09 09:43:53 -0300

    Conexão com a API - Verificando se token é inválido

--- src/services/api.ts
+++ src/services/api.ts
@@ -13,12 +13,23 @@ const api = axios.create({
 
 api.registerInterceptTokenManager = signOut => {
 
-  const interceptTokenManager = api.interceptors.response.use(response => response, error => {
-    if(error.response && error.response.data){
-      return Promise.reject(new AppError(error.response.data.message));
+  const interceptTokenManager = api.interceptors.response.use(response => response, requestError => {
+    if(requestError?.response?.status === 401){
+      if(requestError.response.data?.message === 'token.expired' || requestError.response.data?.message === 'token.invalid'){
+
+      }
+
+      signOut();
     }
 
-    return Promise.reject(error);
+
+
+
+    if(requestError.response && requestError.response.data){
+      return Promise.reject(new AppError(requestError.response.data.message));
+    }
+
+    return Promise.reject(requestError);
   });
 
   return () => {
