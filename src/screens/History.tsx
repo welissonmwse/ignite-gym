428d0428eb85e4c986c4ecb49b6b0ff8d5f2039c
Welisson Silveira <welissonmwse@gmail.com>
2023-01-09 11:17:30 -0300

    Conexão com a API - Exibindo mensagem quando não há hitórico

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useState } from 'react';
-import { Heading, SectionList, Text, useToast, VStack } from 'native-base';
+import { Center, Heading, SectionList, Text, useToast, VStack } from 'native-base';
 
 import { useAuth } from '@hooks/useAuth';
 
@@ -50,28 +50,31 @@ export function History(){
       {
         isLoading ? (
           <Loading />
-        ) : exercises?.length && (
-          <SectionList
-            sections={exercises}
-            keyExtractor={item => item.id}
-            renderItem={({item}) => (
-              <HistoryCard data={item} />
-            )}
-            renderSectionHeader={({section}) => (
-              <Heading color="gray.200" fontSize="md" fontFamily="heading" mt={10} mb={3}>
-                {section.title}
-              </Heading>
-            )}
-            px={8}
-            contentContainerStyle={exercises.length === 0 && { flex: 1, justifyContent: 'center'}}
-            ListEmptyComponent={() => (
+        ) : (
+          exercises?.length > 0 ? (
+            <SectionList
+              sections={exercises}
+              keyExtractor={item => item.id}
+              renderItem={({item}) => (
+                <HistoryCard data={item} />
+              )}
+              renderSectionHeader={({section}) => (
+                <Heading color="gray.200" fontSize="md" fontFamily="heading" mt={10} mb={3}>
+                  {section.title}
+                </Heading>
+              )}
+              px={8}
+              contentContainerStyle={exercises.length === 0 && { flex: 1, justifyContent: 'center'}}
+              showsVerticalScrollIndicator={false}
+            />
+          ) : (
+            <Center flex={1}>
               <Text color="gray.100" textAlign="center">
                 Não há exercícios registrados ainda. {'\n'}
                 Vamos fazer exercícios hoje?
               </Text>
-            )}
-            showsVerticalScrollIndicator={false}
-          />
+            </Center>
+          )
         )
       }
     </VStack>
